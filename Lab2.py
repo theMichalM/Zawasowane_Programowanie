95f112144946b0856757141879dc728d33a6f527
Michał Mazur <michal.mazur4@edu.eukat.pl>
2021-11-09 15:23:46 +0100

    Poprawne z pep8

--- Lab2.py
+++ Lab2.py
@@ -1,31 +1,54 @@
-#Zadanie 2.a.
-def zada(lista):
+# Zadanie 2.a.
+def zada(lista: list):
     for x in lista:
         print(x)
-l1 = ["Michał","Ada","Aneta","Mariusz","Anna"]
+
+
+l1 = ["Michał", "Ada", "Aneta", "Mariusz", "Anna"]
 zada(l1)
 
-#Zadanie 2.b.
-def zadb1(lista):
+
+# Zadanie 2.b.
+def zadb1(lista: list):
     for x in lista:
         print(x*2)
-l2=[1,2,3,4,5]
+
+
+l2 = [1, 2, 3, 4, 5]
 zadb1(l2)
 
-def zadb2(lista):
+
+def zadb2(lista: list):
     listab = [2*x for x in lista]
     print(listab)
+
+
 zadb2(l2)
 
-#Zadanie 2.c.
+
+# Zadanie 2.c.
 def zadc():
     for x in range(10):
-        if x%2==0:
+        if x % 2 == 0:
             print(x)
+
+
 zadc()
 
-#Zadanie 2.d.
+
+# Zadanie 2.d.
 def zadd():
-    for x in range(0,10,2):
+    for x in range(0, 10, 2):
         print(x)
+
+
 zadd()
+
+
+def zadd2(lista: list):
+    for count, x in enumerate(lista):
+        if count % 2 == 1:
+            print(x)
+
+
+zadd2(l2)
